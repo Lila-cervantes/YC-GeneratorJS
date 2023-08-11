86d0f2c56112d9d1529791e5e35072fe33ce175e
Yadira Cervantes <cervantes.lila14@gmail.com>
2023-08-10 21:24:05 -0500

    adding for loops

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,8 @@
-// Assignment Code
+//Here is a list of what the password can be created out of.
 var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var symbols = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~", "_", "\\"]
-
-// prompt()
-
-
-
-
-//confirm() and alert() for variables
-
-
 
 var generateBtn = document.querySelector("#generate");
 
@@ -21,7 +12,7 @@ function generatePassword(){
 if (passwordCharacters < 8 || passwordCharacters > 128) {
 alert('the password is not the required length!')
 return
-} //
+} 
   var withNumbers = confirm("Do you want to use numbers?");
 
   var withLowerCase = confirm("Do you want lowercase characters?");
@@ -30,48 +21,29 @@ return
 
   var withSymbols = confirm("Do you want symbols?");
 
-  
-
   if(withNumbers === false && withLowerCase === false && withUpperCase === false && withSymbols === false){
-    alert('please make atleast one selection')
+    alert('Please make atleast one selection.')
     return
   }
-  // if (withNumbers) {
-  //   alert("Password will have numbers.")
-  // }
-  // else {
-  //   alert("Password will not have numbers.")
-  // }
-  
-  
-  // if (withLowerCase) {
-  //   alert("Password will have lowercase characters.") 
-  // }
-  // else {
-  //   alert("Passwrod will not have lowercase characters.")
-  // }
-  
-  
-  // if(withUpperCase) {
-  //   alert("Password will have uppercase characters.")
-  // }
-  // else {
-  //   alert("Password will not have uppercase characters.")
-  // }
-  
-  
-  // if(withSymbols) {
-  //   alert("Password will have symbols.") 
-  // }
-  // else {
-  //   alert("Password will not have symbols.")
-  // } //
-} 
 
+}
+var userOptions {
+  passwordCharacters: passwordCharacters, 
+  withNumbers: withNumbers,
+  withLowerCase: withLowerCase,
+  withUpperCase: withUpperCase,
+  withSymbols: withSymbols
+}
+var index = Math.floor(Math.random() * numberCharacters.length);
+var computerChoice = options[index];
+
+for (let i = 0; i < passwordCharacters; i++) {
+  let rng =[Math.floor(Math.random() * possibleCharacters.length)];
+  finalPassword = finalPassword + possibleCharacters[rng];
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
